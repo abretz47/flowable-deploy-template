28add2d864b84b0daaf32993c7e7f320e5035882
abretz47 <abretz47@gmail.com>
2026-04-23 09:47:32 -0500

    Fix SSH key handling and permissions in create-env.sh

--- create-env.sh
+++ create-env.sh
@@ -28,19 +28,26 @@ setup_cluster() {
 	if [ ! -f "/home/codespace/.ssh/id_rsa" ]; then
 		mkdir -p /home/codespace/.ssh
 		ssh-keygen -t rsa -b 4096 -f /home/codespace/.ssh/id_rsa -P ""
-		
+
 		export GITHUB_TOKEN="" 
 		echo $ARC_TOKEN | gh auth login -p https --with-token
 		gh auth refresh -h github.com -s admin:public_key
 		gh ssh-key add /home/codespace/.ssh/id_rsa.pub --title "${CODESPACE_NAME}" --type authentication
+	fi
 
+	if [ ! -f "docker/.ssh/id_rsa" ]; then
 		mkdir -p docker/.ssh
 		cp /home/codespace/.ssh/id_rsa.pub docker/.ssh/
 		cp /home/codespace/.ssh/id_rsa docker/.ssh/
+		cp /home/codespace/.ssh/id_ed25519.pub docker/.ssh/
+		cp /home/codespace/.ssh/id_ed25519 docker/.ssh/
 		echo "github.com ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCj7ndNxQowgcQnjshcLrqPEiiphnt+VTTvDP6mHBL9j1aNUkY4Ue1gvwnGLVlOhGeYrnZaMgRK6+PKCUXaDbC7qtbW8gIkhL7aGCsOr/C56SJMy/BCZfxd1nWzAOxSDPgVsmerOBYfNqltV9/hWCqBywINIR+5dIg6JTJ72pcEpEjcYgXkE2YEFXV1JHnsKgbLWNlhScqb2UmyRkQyytRLtL+38TGxkxCflmO+5Z8CSSNY7GidjMIZ7Q4zMjA2n1nGrlTDkzwDCsw+wqFPGQA179cnfGWOWRVruj16z6XyvxvjJwbz0wQZ75XK5tKSb7FNyeIEs4TT4jk+S4dhPeAUC5y+bDYirYgM4GC7uEnztnZyaVWQ7B381AK4Qdrwt51ZqExKbQpTUNn+EjqoTwvqNj4kqx5QUCI0ThS/YkOxJCXmPUWZbhjpCg56i+2aB6CmK2JGhn57K5mj0MNdBXA4/WnwH6XoPWJzK5Nyu2zB3nAZp+S5hpQs+p1vN1/wsjk=" >> docker/.ssh/known_hosts
+		echo "github.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl" >> docker/.ssh/known_hosts
 		sudo chmod +rw docker/.ssh
+		sudo chown 100:100 docker/.ssh/id_ed25519
+		sudo chown 100:100 docker/.ssh/id_ed25519.pub
 		sudo chown 100:100 docker/.ssh/id_rsa
-		sudo chown 100:100 docker/.ssh/id_rsa
+		sudo chown 100:100 docker/.ssh/id_rsa.pub
 		sudo chown 100:100 docker/.ssh/known_hosts
 	fi
 
